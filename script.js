ef34c3e01bed1bd3b69616c133deee3f8f49ee30
arehmankhan613-create <arehmankhan613@gmail.com>
2026-08-01 22:52:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,4 +54,43 @@ document.getElementById("icon").src =
     "Wind: " + data.wind.speed + " km/h";
 
 
+}async function searchCity(){
+
+let city = document.getElementById("cityInput").value;
+
+
+let url =
+`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+
+let response = await fetch(url);
+
+let data = await response.json();
+
+
+document.getElementById("city").innerHTML =
+data.name + ", " + data.sys.country;
+
+
+document.getElementById("temp").innerHTML =
+data.main.temp + " °C";
+
+
+document.getElementById("condition").innerHTML =
+data.weather[0].description;
+
+
+document.getElementById("humidity").innerHTML =
+"Humidity: " + data.main.humidity + "%";
+
+
+document.getElementById("wind").innerHTML =
+"Wind: " + data.wind.speed + " km/h";
+
+
+document.getElementById("icon").src =
+"https://openweathermap.org/img/wn/" 
++ data.weather[0].icon + "@2x.png";
+
+
 }
