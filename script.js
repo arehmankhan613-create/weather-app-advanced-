4eb07f57bb59c6b174d7113acab83c1efbbf89ba
arehmankhan613-create <arehmankhan613@gmail.com>
2026-08-02 00:02:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const apiKey = "3d12bfd7c67376c39b298450c321d916";
 
 function getLocation(){
 
-navigator.geolocation.getCurrentPosition(getWeather);
+    navigator.geolocation.getCurrentPosition(getWeather);
 
 }
 
@@ -11,97 +11,97 @@ navigator.geolocation.getCurrentPosition(getWeather);
 
 async function getWeather(position){
 
-let lat = position.coords.latitude;
-let lon = position.coords.longitude;
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
 
 
-let url =
-`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+    let url =
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
 
-let response = await fetch(url);
+    let response = await fetch(url);
 
-let data = await response.json();
+    let data = await response.json();
 
 
-showWeather(data);
+    showWeather(data);
+
+    getForecast(data.name);
 
 }
+
 
 
 
 async function searchCity(){
 
-let city = document.getElementById("cityInput").value;
+    let city = document.getElementById("cityInput").value;
 
 
-if(city==""){
+    if(city==""){
 
-alert("Enter city name");
-return;
+        alert("Enter city name");
+        return;
 
-}
-
-
-let url =
-`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    }
 
 
-let response = await fetch(url);
-
-let data = await response.json();
-
-
-if(data.cod=="404"){
-
-alert("City not found");
-return;
-
-}
+    let url =
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
 
-showWeather(data);document.getElementById("wind").innerHTML =
-"Wind: " + data.wind.speed + " km/h";
+    let response = await fetch(url);
 
-getForecast(data.name);
-}
+    let data = await response.json();
 
-getForecast(city);
 
+    if(data.cod=="404"){
+
+        alert("City not found");
+        return;
+
+    }
+
+
+    showWeather(data);
+
+    getForecast(data.name);
 
 }
+
 
 
 
 function showWeather(data){
 
 
-document.getElementById("city").innerHTML =
-data.name + ", " + data.sys.country;
+    document.getElementById("city").innerHTML =
+    data.name + ", " + data.sys.country;
 
 
-document.getElementById("temp").innerHTML =
-data.main.temp + " °C";
+    document.getElementById("temp").innerHTML =
+    data.main.temp + " °C";
 
 
-document.getElementById("condition").innerHTML =
-data.weather[0].description;
+    document.getElementById("condition").innerHTML =
+    data.weather[0].description;
 
 
-document.getElementById("icon").src =
-"https://openweathermap.org/img/wn/"
-+ data.weather[0].icon + "@2x.png";
+    document.getElementById("icon").src =
+    "https://openweathermap.org/img/wn/"
+    + data.weather[0].icon + "@2x.png";
 
 
-document.getElementById("humidity").innerHTML =
-"Humidity: " + data.main.humidity + "%";
+    document.getElementById("humidity").innerHTML =
+    "Humidity: " + data.main.humidity + "%";
 
 
-document.getElementById("wind").innerHTML =
-"Wind: " + data.wind.speed + " km/h";
+    document.getElementById("wind").innerHTML =
+    "Wind: " + data.wind.speed + " km/h";
 
 
 }
+
 
 
 
@@ -109,41 +109,41 @@ document.getElementById("wind").innerHTML =
 async function getForecast(city){
 
 
-let url =
-`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
+    let url =
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
 
 
-let response = await fetch(url);
+    let response = await fetch(url);
 
-let data = await response.json();
-
-
-let box = document.getElementById("forecast");
-
-box.innerHTML="";
+    let data = await response.json();
 
 
-for(let i=0;i<5;i++){
+    let box = document.getElementById("forecast");
+
+    box.innerHTML = "";
 
 
-let item=data.list[i*8];
+    for(let i=0;i<5;i++){
 
 
-box.innerHTML += `
+        let item = data.list[i*8];
 
-<div class="card">
 
-<p>${item.dt_txt.split(" ")[0]}</p>
+        box.innerHTML += `
 
-<img src="https://openweathermap.org/img/wn/${item.weather[0].icon}.png">
+        <div class="card">
 
-<p>${item.main.temp}°C</p>
+        <p>${item.dt_txt.split(" ")[0]}</p>
 
-</div>
+        <img src="https://openweathermap.org/img/wn/${item.weather[0].icon}.png">
 
-`;
+        <p>${item.main.temp}°C</p>
 
-}
+        </div>
+
+        `;
+
+    }
 
 
 }
